397b30e8bba013d9ea9a5f9a99e0712378c43b98
Szymon Boniszewski <szymkop2001@gmail.com>
2022-10-26 00:38:39 +0200

    Min/Max fixed

--- JavaCw2/JavaRandom2DArray.java
+++ JavaCw2/JavaRandom2DArray.java
@@ -4,40 +4,62 @@ import java.util.Random;
 
 public class JavaRandom2DArray {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         int n =50;
         double[][] wartosci = new double[4][n];
-        
+        double pomoc;
         Random rand = new Random();
         
         double otwarcie = 40 + (70-40) * rand.nextDouble();
         
         wartosci[0][0]=otwarcie;
         wartosci[1][0]=(otwarcie-otwarcie*0.03) + ((otwarcie+otwarcie*0.03)-(otwarcie-otwarcie*0.03)) * rand.nextDouble();
-        wartosci[2][0]=(otwarcie-otwarcie*0.05) + ((otwarcie+otwarcie*0.05)-(otwarcie-otwarcie*0.05)) * rand.nextDouble();
-        wartosci[3][0]=(otwarcie-otwarcie*0.05) + ((otwarcie+otwarcie*0.05)-(otwarcie-otwarcie*0.05)) * rand.nextDouble();
+        do{
+            wartosci[2][0]=(otwarcie-otwarcie*0.05) + ((otwarcie+otwarcie*0.05)-(otwarcie-otwarcie*0.05)) * rand.nextDouble();
+        }
+        while(wartosci[1][0]<wartosci[2][0]);
+        
+        do{
+            wartosci[3][0]=(otwarcie-otwarcie*0.05) + ((otwarcie+otwarcie*0.05)-(otwarcie-otwarcie*0.05)) * rand.nextDouble();
+        }
+        while(wartosci[1][0]>wartosci[3][0]);
+
         if(wartosci[2][0]>wartosci[3][0])
             {
-                double pomoc = wartosci[2][0];
+                pomoc = wartosci[2][0];
                 wartosci[2][0] = wartosci[3][0];
                 wartosci[3][0] = pomoc;
             }
+        
+        
         for(int i=1;i<wartosci[0].length;i++)
         {
             double otwarcieNext = wartosci[1][i-1];
             wartosci[0][i]= otwarcieNext;
-            wartosci[1][i]=(otwarcieNext-otwarcieNext*0.03) + ((otwarcieNext+otwarcieNext*0.05)-(otwarcieNext-otwarcieNext*0.05)) * rand.nextDouble();
-            wartosci[2][i]=(otwarcieNext-otwarcieNext*0.05) + ((otwarcieNext+otwarcieNext*0.05)-(otwarcieNext-otwarcieNext*0.05)) * rand.nextDouble();
-            wartosci[3][i]=(otwarcieNext-otwarcieNext*0.05) + ((otwarcieNext+otwarcieNext*0.05)-(otwarcieNext-otwarcieNext*0.05)) * rand.nextDouble();
+            wartosci[1][i]=(otwarcieNext-otwarcieNext*0.03) + ((otwarcieNext+otwarcieNext*0.03)-(otwarcieNext-otwarcieNext*0.03)) * rand.nextDouble();
+            do{
+                wartosci[2][i]=(otwarcieNext-otwarcieNext*0.05) + ((otwarcieNext+otwarcieNext*0.05)-(otwarcieNext-otwarcieNext*0.05)) * rand.nextDouble();
+            }
+            while(wartosci[1][i]<wartosci[2][i]);
+
+            do{
+                wartosci[3][i]=(otwarcieNext-otwarcieNext*0.05) + ((otwarcieNext+otwarcieNext*0.05)-(otwarcieNext-otwarcieNext*0.05)) * rand.nextDouble();
+            }
+            while(wartosci[1][i]>wartosci[3][i]);
             if(wartosci[2][i]>wartosci[3][i])
             {
-                double pomoc = wartosci[2][i];
+                pomoc = wartosci[2][i];
                 wartosci[2][i] = wartosci[3][i];
                 wartosci[3][i] = pomoc;
             }
+        }
+
+        for(int i=1; i<wartosci[0].length;i++)
+        {
+            System.out.println("Open: "+wartosci[0][i]);
+            System.out.println("Close: "+wartosci[1][i]);
+            System.out.println("Min: "+wartosci[2][i]);
+            System.out.println("Max: "+wartosci[3][i]);
         }
             
     }
