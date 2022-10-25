d5d5dc797b5505d07df1d5a0f12d79d271341b33
Szymon Boniszewski <103605388+DeSpecu@users.noreply.github.com>
2022-10-25 14:24:25 +0200

    AllConditionsMet

--- Python/Python Lab 4/ExDigitsFromString.py
+++ Python/Python Lab 4/ExDigitsFromString.py
@@ -1,12 +1,39 @@
+
 def stringToInt(text=""):
 
-    arrayForDigit = []
+    dataForDigit = ""
 
     for c in text:
         if c.isdigit() or c in ['+','-','e']:
-            arrayForDigit.append(c)
+            dataForDigit += c
         else:
             break
-    for ch in arrayForDigit:
-        
+    return dataForDigit
 
+text = input("Podaj tekst\n")
+data = stringToInt(text)
+
+output = ""
+power=""
+
+if len(data)>0:
+    if data[0]=="-":
+        output+="-"
+    else:
+        output+=data[0]
+    for x in range(1,len(data)):
+        if data[x]=="-":
+            break
+        if data[x].isdigit():
+            output += data[x]
+        if data[x] == "e" and x<len(data)-1:
+            power = data[x + 1:]
+            if power.count("-")==0:
+                output = int(output) * (10 ** int(power))
+            break
+
+elif len(data)==0:
+    output="0";
+
+
+print(output)
