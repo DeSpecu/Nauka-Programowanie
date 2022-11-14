18f00d42747b3ad0cac62a030eee1c94dca2375f
Szymon Boniszewski <szymkop2001@gmail.com>
2022-11-14 18:29:12 +0100

    changed

--- Python/Python Lab 5/CodeFile.py
+++ Python/Python Lab 5/CodeFile.py
@@ -1,6 +1,7 @@
 def read_file(file_name):
     lines = open(file_name).read()
     return lines
+
 def save(file_name, dane):
     file = open(file_name, 'a+')
     file.write(str(dane))
@@ -12,18 +13,24 @@ def szyfruj(text:str, n:int)->str:
     toChange = list(text)
 
     while True:
-        value = ord(toChange[i]) + shift
-        if value > 125:
-            value -= 92
-        toChange[i] = chr(value)
-        i += 1
-        if i >= len(text):
-            break
+        if toChange[i] not in [" ", "\n", "\t"]:
+            value = ord(toChange[i]) + shift
+            if value > 125:
+                value -= 92
+            toChange[i] = chr(value)
+            i += 1
+            if i >= len(text):
+                break
+        else:
+            i+=1
+            if i >= len(text):
+                break
     text = "".join(toChange)
     return text
-p = "C:/Users/student8.UE/Desktop/tak.txt"
+
+p = "C:/Users/szymk/Desktop/test.txt"
+dane = read_file(p)
 p=p.split('/')
 p="/".join(p[:-1]+["_"+p[-1]])
-dane = read_file("C:/Users/student8.UE/Desktop/tak.txt")
 n = input("Przesuniecie\n")
 save(p, szyfruj(dane, int(n)))
