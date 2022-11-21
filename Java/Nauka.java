3724918a77d1c3cbc005f1161bb6a33edac3245b
Szymon Boniszewski <szymkop2001@gmail.com>
2022-11-21 02:14:04 +0100

    changes

---
+++ Java/Nauka.java
@@ -0,0 +1,84 @@
+package Java;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Nauka {
+
+    public static void main(String[] args)
+    {
+        Scanner wejscie = new Scanner(System.in);
+        // int a = Integer.parseInt(wejscie.nextLine());
+        // int b = Integer.parseInt(wejscie.nextLine());
+        // wejscie.close();
+
+        // if(a>10 && b>10){
+        //     System.out.println(a+b);
+        // }
+        // else if(a<10 && b<10 && a>0 && b>0){
+        //     System.out.println(a-b);
+        // }
+        // else if((a>0 && b<0)||(a<0 && b>0)){
+        //     System.out.println(b-a);
+        // }
+        // else if(a<0 && b<0){
+        //     System.out.println("Obie ujemne");
+        // }
+
+
+        // int nowy = wejscie.nextInt();
+        // if(nowy>10 || nowy<0)
+        // {
+        //     System.out.println("Pusto");
+        // }
+        // for(int i=1; i<5; i++)
+        // {
+        //     String wyjscie = String.valueOf(nowy);
+        //     System.out.println(wyjscie.repeat(i));
+        // }
+
+
+        // double n = wejscie.nextDouble();
+        // double suma = 0.0;
+        // int x;
+        // while(n!=0)
+        // {
+        //     x = (int)n;
+        //     n = n-x;
+        //     suma = suma+n;
+        //     n = wejscie.nextDouble();
+        // }
+        // System.out.println(suma);
+
+        
+        // String n = wejscie.nextLine();
+        // char c = n.charAt(0);
+        // System.out.println(c);
+        // char[] chars = {'a','o','i','e','u','y'};
+        // int ile = 0;
+        // while(c!='k')
+        // {
+        //     boolean contains = false;
+        //     for (char ch : chars) 
+        //     {
+        //         if (ch == c) 
+        //         {
+        //         contains = true;
+        //         break;
+        //         }
+        //     }
+            
+        //     if(contains)
+        //     {
+        //         ile = ile +1;
+        //     }
+        //     n = wejscie.nextLine();
+        //     c = n.charAt(0);
+        // } 
+        // System.out.println(ile);
+
+
+        
+    }
+}
--- Python/Python Lab 5/ChartLetterInFile.py
+++ Python/Python Lab 5/ChartLetterInFile.py
@@ -13,9 +13,9 @@ def count(data:str)->dict:
 
 def chart(data:dict):
     data = dict(sorted(data.items()))
+    m = max(data, key=data.get)
+    mValue = data[m]
     for x in range(len(data)):
-        m = max(data, key=data.get)
-        mValue = data[m]
         key = list(data.keys())[x]
         amount = 50*data[key]/mValue
         print(f"{key} {'*'*int(amount)} {' '*(50-int(amount))} {data[key]}")
