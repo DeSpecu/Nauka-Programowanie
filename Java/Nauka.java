e09ce5215ba64f4645c9a7bd842c89ab72afbb3a
Szymon Boniszewski <szymkop2001@gmail.com>
2022-11-21 10:08:51 +0100

    kolosy

--- Java/Nauka.java
+++ Java/Nauka.java
@@ -1,6 +1,9 @@
 package Java;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 
@@ -79,6 +82,91 @@ public class Nauka {
         // System.out.println(ile);
 
 
-        
+        //Kolos zadanie 1!!!!!!!!!!
+        // String dane = wejscie.nextLine();
+
+        // int a = wejscie.nextInt();
+        // while(a<1 || a > dane.length()-2)
+        // {
+        //     System.out.println("Podaj liczbę z zakresu 1 - długość napisu-2");
+        //     a = wejscie.nextInt();
+        // }
+        // int b = wejscie.nextInt();
+        // while(b<a || b > dane.length()-2)
+        // {
+        //     System.out.println("Podaj liczbę z zakresu 1 - długość napisu-2 i większą od a");
+        //     b = wejscie.nextInt();
+        // }
+        // List<String> wyjscie = new LinkedList<String>();
+
+        // String zeroA="";
+        // String adoB="";
+        // String bKoniec="";
+        // char[] petla = dane.toCharArray();
+        // int doascii;
+        // for(int i = 0; i<dane.length(); i++)
+        // {
+        //     if(i<=a)
+        //     {
+        //         zeroA=zeroA+petla[i];
+        //     }
+        //     else if(i>=a && i<=b)
+        //     {
+        //         if((int)petla[i]>96 && (int)petla[i]<123)
+        //         {
+        //             doascii=petla[i]-32;
+        //             adoB=adoB+(char)doascii;
+        //         }
+        //         else{
+        //             adoB=adoB + petla[i];
+        //         }
+        //     }
+        //     else if(i>=b)
+        //     {
+        //         bKoniec=bKoniec+petla[i];
+        //     }
+        // }
+        // wyjscie.add(zeroA);
+        // wyjscie.add(adoB);
+        // wyjscie.add(bKoniec);
+
+        // for(String s : wyjscie)
+        // {
+        //     System.out.println(s);
+        // }
+
+
+        //Zadanie 2
+        // int k = wejscie.nextInt();
+        // int n = wejscie.nextInt();
+        // int m = wejscie.nextInt();
+        // int losowa;
+        // Random rand = new Random();
+        // Boolean[] tablica = new Boolean[k];
+        // for(int i =0; i<tablica.length; i++)
+        // {
+        //     losowa = rand.nextInt(1,5);
+        //     if(losowa<4)
+        //     {
+        //         tablica[i]=true;
+        //     }
+        //     else
+        //     {
+        //         tablica[i]=false;
+        //     }
+        // }
+        // Boolean[][] tablica2d = new Boolean[n][m];
+        // if(n*m==k)
+        // {
+        //     int indeks = 0;
+        //     for(int i =0; i<n; i++)
+        //     {
+        //         for(int j =0; j<m; j++)
+        //         {
+        //             tablica2d[i][j] = tablica[indeks];
+        //             indeks++;
+        //         }
+        //     }
+        // }
     }
 }
