ebf5fcf2b36aa554b64ef64f8b91910d224da298
Szymon Boniszewski <103605388+DeSpecu@users.noreply.github.com>
2022-12-11 21:47:50 +0100

    Update kolko_i_krzyzyk.py
    3x3 works

--- Python/Python Lab 6/kolko_i_krzyzyk.py
+++ Python/Python Lab 6/kolko_i_krzyzyk.py
@@ -17,24 +17,28 @@ class Plansza:
             return "X"
         return "O"
  
-    def sprawdz_kolumne(self):
+    def sprawdz(self):
+        
         for x in range(self.wielkosc):
-            for y in range(self.wielkosc):
-                if self.__plansza[x][0] == self.__plansza[x+1][0]:
-                    return False
-                if self.__plansza[x][y] != self.__plansza[x][y+1]:
-                    return False      
-        return True
-
-    def sprawdz_wiersz(self):
-        for x in range(self.wielkosc):
-            for y in range(self.wielkosc-1):
-                if self.__plansza[x][y] != self.__plansza[x][y+1]:
-                    return False    
+            if "-" != self.__plansza[x][0] == self.__plansza[x][1] == self.__plansza[x][2] or "-" != self.__plansza[0][x] == self.__plansza[1][x] == self.__plansza[2][x]:
+                return True
+        if "-" != self.__plansza[0][0] == self.__plansza[1][1] == self.__plansza[2][2] or "-" != self.__plansza[0][2] == self.__plansza[1][1] == self.__plansza[2][0]:
+            return True
+        
+        return False
+    
+    def czypuste(self,x, y):
+        if self.__plansza[int(x)-1][int(y)-1] != "-":
+            return False
         return True
  
     def zagraj(self):
+        tury = 0
         while True:
+            tury+=1
+            if tury>9:
+                print("Remis")
+                break
             self.wyswietl()
             print(f"Tura {self.symbol()}")
             wiersz = input("Podaj wiersz\n")
@@ -43,14 +47,18 @@ class Plansza:
             kolumna = input("Podaj kolumnę\n")
             while int(kolumna) > self.wielkosc:
                 kolumna = input(f"Podaj kolumnę mniejszą od {self.wielkosc}\n")
-            self.__plansza[int(wiersz)-1][int(kolumna)-1] = self.symbol()
+            if self.czypuste(wiersz,kolumna):
+                self.__plansza[int(wiersz)-1][int(kolumna)-1] = self.symbol()
+            else:
+                print("Miejsce zajęte")
+                break
             self.wyswietl()
             self.__symbol = not self.__symbol
-            if self.sprawdz_kolumne():
+            if self.sprawdz():
                 self.__symbol = not self.__symbol
                 print(f"Wygrał {self.symbol()}")
                 break
-
-rozmiar = input("Podaj wielkość planszy\n")
-p = Plansza(int(rozmiar))
+#Do kolejnego zadania
+#rozmiar = input("Podaj wielkość planszy\n")
+p = Plansza(3)
 p.zagraj()
