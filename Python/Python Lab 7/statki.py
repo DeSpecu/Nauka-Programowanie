774b5bf4ee03a00d06bb52fcfb58404d32745325
Szymon Boniszewski <103605388+DeSpecu@users.noreply.github.com>
2022-12-19 12:19:03 +0100

    tak

---
+++ Python/Python Lab 7/statki.py
@@ -0,0 +1,77 @@
+class Statek:
+    def __init__(self, wspolrzedne1, wspolrzedne2) -> None:
+        self.wspolrzedne1 = wspolrzedne1
+        self.wspolrzedne2 = wspolrzedne2
+        self.maszt = True
+    
+    def strzal(self, wsp1, wsp2):
+        if wsp1 == self.wspolrzedne1 and wsp2 == self.wspolrzedne2:
+            self.maszt = False
+        
+        if not self.maszt:
+            return 2
+        else:
+            return 0
+        
+    def wyswietl(self):
+        print(f"Maszt 1: {self.wspolrzedne1}, {self.wspolrzedne2}")
+    
+
+class Statek2(Statek):
+    def __init__(self, wspolrzedne1, wspolrzedne2) -> None:
+        super().__init__(wspolrzedne1, wspolrzedne2)
+        self.maszt = True
+        self.maszt2 = True
+
+    def strzal(self, wsp1, wsp2):
+        if wsp1 == self.wspolrzedne1 and wsp2 == self.wspolrzedne2:
+            self.maszt = False
+            return 1
+        elif wsp1 == self.wspolrzedne1 and wsp2 == self.wspolrzedne2+1:
+            self.maszt2 = False
+            return 1
+        if not self.maszt and not self.maszt2:
+            return 2
+        else:
+            return 0
+        
+    def wyswietl(self):
+        print(f"Maszt 1: {self.wspolrzedne1}, {self.wspolrzedne2}  Maszt 2: {self.wspolrzedne1}, {self.wspolrzedne2+1}")
+
+class Statek3(Statek):
+    def __init__(self, wspolrzedne1, wspolrzedne2) -> None:
+        super().__init__(wspolrzedne1, wspolrzedne2)
+        self.maszt = True
+        self.maszt2 = True
+        self.maszt3 = True
+
+    def strzal(self, wsp1, wsp2):
+        if wsp1 == self.wspolrzedne1 and wsp2 == self.wspolrzedne2:
+            self.maszt = False
+            return 1
+        elif wsp1 == self.wspolrzedne1 and wsp2 == self.wspolrzedne2+1:
+            self.maszt2 = False
+            return 1
+        elif wsp1 == self.wspolrzedne1 and wsp2 == self.wspolrzedne2+2:
+            self.maszt3 = False
+            return 1
+        if not self.maszt and not self.maszt2 and not self.maszt3:
+            return 2
+        return 0
+
+    def wyswietl(self):
+        print(f"Maszt 1: {self.wspolrzedne1}, {self.wspolrzedne2}  Maszt 2: {self.wspolrzedne1}, {self.wspolrzedne2+1}  Maszt 3: {self.wspolrzedne1}, {self.wspolrzedne2+2}")
+
+s1 = Statek(1,1)
+s2 = Statek2(2,1)        
+s3 = Statek3(3,1)
+
+print(s1.strzal(1,1))
+print(s2.strzal(2,2))
+print(s3.strzal(3,1))
+print(s3.strzal(3,2))
+print(s3.strzal(3,3))
+
+s1.wyswietl()
+s2.wyswietl()
+s3.wyswietl()
